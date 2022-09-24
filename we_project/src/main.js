866ce4630f7159cd08f7c8748f245092bf08148f
eunjiP <dae4227@naver.com>
2022-09-24 16:59:51 +0900

    기본셋팅

--- we_project/src/main.js
+++ we_project/src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import mixins from './mixin'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+.use(router)
+.use(VueSweetalert2)
+.mixin(mixins)
+.mount('#app')
